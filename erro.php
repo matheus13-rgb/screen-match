6b71057d3054ce57c4a50144c4c8fef9c36de3b4
duarte <matheus240377@gmail.com>
2025-06-18 16:28:41 -0300

    Avaliação negativa

--- erro.php
+++ erro.php
@@ -10,5 +10,12 @@ require 'autoload.php';
 $serie = new Serie('Nome da serie', 2024, Genero::Acao, 7, 20, 40);
 $episodio = new Episodio($serie, 'Piloto', 1);
 
-$conversor = new ConversorNotaEstrela();
-echo $conversor->converte($episodio);
+try {
+    $episodio->avalia(45);
+    $episodio->avalia(-35);
+
+    $conversor = new ConversorNotaEstrela();
+    echo $conversor->converte($episodio);
+} catch (Exception $e) {
+    echo "Um problema aconteceu: " . $e->getMessage();
+}
--- src/Calculations/ConversorNotaEstrela.php
+++ src/Calculations/ConversorNotaEstrela.php
@@ -10,10 +10,10 @@ class ConversorNotaEstrela
     public function converte(Avaliavel $avaliavel): float
     {
         try {
-        $nota = $avaliavel->media();
+            $nota = $avaliavel->media();
 
-        return round($nota) / 2;
-        } catch(DivisionByZeroError $erro){
+            return round($nota) / 2;
+        } catch(DivisionByZeroError) {
             return 0;
         }
     }
